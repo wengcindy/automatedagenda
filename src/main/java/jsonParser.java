cb5c1a73498fca2128cafc7b83f99a8c3b0a05b0
Charles Lyu <liang.lyu@duke.edu>
2019-06-04 14:52:10 -0400

    Mass process all sessions in one JSON file automatically

--- src/main/java/jsonParser.java
+++ src/main/java/jsonParser.java
@@ -19,147 +19,61 @@ public class jsonParser
     { 
 
         Object obj = null;
-		try {
+        JSONObject database = null;
+        try {
 			obj = new JSONParser().parse(new FileReader("2019winter.json"));
-		} catch (FileNotFoundException e) {
+			database = (JSONObject) obj;
+		} catch (IOException | ParseException | ClassCastException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		
-		// Set up file and file writer
-		File winter1 = new File("winter1.csv");
-		winter1.createNewFile();
-		File winter2 = new File("winter2.csv");
-		winter2.createNewFile();
-		File winter3 = new File("winter3.csv");
-		winter3.createNewFile();
-        FileWriter csvWriter1 = new FileWriter(winter1); 
-        FileWriter csvWriter2 = new FileWriter(winter2); 
-        FileWriter csvWriter3 = new FileWriter(winter3); 
+		}
 
-        
-        // Create column labels for csv
-        csvWriter1.append("Name");  
-        csvWriter1.append(",");  
-        csvWriter1.append("Individual speaking time");  
-        csvWriter1.append(",");  
-        csvWriter1.append("Total number of people spoken");  
-        csvWriter1.append(",");  
-        csvWriter1.append("Total speaking time");   
-        csvWriter1.append("\n");
-        
-        csvWriter2.append("Name");  
-        csvWriter2.append(",");  
-        csvWriter2.append("Individual speaking time");  
-        csvWriter2.append(",");  
-        csvWriter2.append("Total number of people spoken");  
-        csvWriter2.append(",");  
-        csvWriter2.append("Total speaking time");   
-        csvWriter2.append("\n");
-        
-        csvWriter3.append("Name");  
-        csvWriter3.append(",");  
-        csvWriter3.append("Individual speaking time");  
-        csvWriter3.append(",");  
-        csvWriter3.append("Total number of people spoken");  
-        csvWriter3.append(",");  
-        csvWriter3.append("Total speaking time");   
-        csvWriter3.append("\n");
+		for (Object sessionObj : database.keySet()) {
+            String sessionName = (String) sessionObj;
+            System.out.println(sessionName);
+            System.out.println("---------");
 
-        JSONObject jo = (JSONObject) obj; 
-        
-        List l = new ArrayList();
-        int numspoken = 0;
-        int timespoken = 0;
-        
-        // Winter 1
-        System.out.println("Winter 1");
-        System.out.println("---------");
-        JSONObject w1 = (JSONObject) jo.get("2019winter1");
-        JSONArray w1_audio = (JSONArray) w1.get("audioData");
-        for(Object o: w1_audio){
-            String username = (String) ((JSONObject) o).get("username");
-            long startTime = ((long) ((JSONObject)o).get("startTime"))/1000;
-            long endTime = ((long) ((JSONObject)o).get("endTime"))/1000;
-            numspoken++;
-            timespoken += endTime - startTime;
-            
-            System.out.println(username);
-            System.out.println(endTime - startTime);
-            System.out.println("Num of ppl spoken: " + numspoken);
-            System.out.println("Total time spoken: " + timespoken);
+            File file = new File(sessionName + ".csv");
+            file.createNewFile();
+            FileWriter csvWriter = new FileWriter(file);
+
+            // Create column labels for csv
+            csvWriter.append("Name");
+            csvWriter.append(",");
+            csvWriter.append("Individual speaking time");
+            csvWriter.append(",");
+            csvWriter.append("Total number of people spoken");
+            csvWriter.append(",");
+            csvWriter.append("Total speaking time");
+            csvWriter.append("\n");
+
+            int numspoken = 0;
+            double timespoken = 0;
+            JSONObject sessionData = (JSONObject) database.get(sessionName);
+            JSONArray sessionAudio = (JSONArray) sessionData.get("audioData");
+            for(Object o: sessionAudio){
+                JSONObject singleSpeech = (JSONObject) o;
+                String username = (String) singleSpeech.get("username");
+                double startTime = ((long) singleSpeech.get("startTime"))/1000.0;
+                double endTime = ((long) singleSpeech.get("endTime"))/1000.0;
+                numspoken++;
+                timespoken += endTime - startTime;
+
+                System.out.println(username);
+                System.out.println(endTime - startTime);
+                System.out.println("Num of ppl spoken: " + numspoken);
+                System.out.println("Total time spoken: " + timespoken);
+                System.out.println();
+
+                //Write to csv file
+                List<String> l = Arrays.asList(username, Double.toString(endTime-startTime), Long.toString(numspoken), Double.toString(timespoken));
+                csvWriter.append(String.join(",", l));
+                csvWriter.append("\n");
+            }
             System.out.println();
-            
-            //Write to csv file
-            l = Arrays.asList(username, Long.toString(endTime-startTime), Long.toString(numspoken), Long.toString(timespoken));
-            csvWriter1.append(String.join(",", l));
-            csvWriter1.append("\n");
-        }
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        
-        numspoken = 0;
-        timespoken = 0;
-        
-        // Winter 2
-        System.out.println("Winter 2");
-        System.out.println("---------");
-        JSONObject w2 = (JSONObject) jo.get("2019winter2");
-        JSONArray w2_audio = (JSONArray) w2.get("audioData");
-        for(Object o: w2_audio){
-            String username = (String) ((JSONObject) o).get("username");
-            long startTime = ((long) ((JSONObject)o).get("startTime"))/1000;
-            long endTime = ((long) ((JSONObject)o).get("endTime"))/1000;
-            numspoken++;
-            timespoken += endTime - startTime;
-            System.out.println(username);
-            System.out.println(endTime - startTime);
-            System.out.println("Num of ppl spoken: " + numspoken);
-            System.out.println("Total time spoken: " + timespoken);
             System.out.println();
-            
-            l = Arrays.asList(username, Long.toString(endTime-startTime), Long.toString(numspoken), Long.toString(timespoken));
-            csvWriter2.append(String.join(",", l));
-            csvWriter2.append("\n");
-        }
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        
-        numspoken = 0;
-        timespoken = 0;
-        
-        // Winter 3
-        System.out.println("Winter 3");
-        System.out.println("---------");
-        JSONObject w3 = (JSONObject) jo.get("2019winter3");
-        JSONArray w3_audio = (JSONArray) w3.get("audioData");
-        for(Object o: w3_audio){
-            String username = (String) ((JSONObject) o).get("username");
-            long startTime = ((long) ((JSONObject)o).get("startTime"))/1000;
-            long endTime = ((long) ((JSONObject)o).get("endTime"))/1000;
-            numspoken++;
-            timespoken += endTime - startTime;
-            System.out.println(username);
-            System.out.println(endTime - startTime);
-            System.out.println("Num of ppl spoken: " + numspoken);
-            System.out.println("Total time spoken: " + timespoken);
             System.out.println();
-            
-            l = Arrays.asList(username, Long.toString(endTime-startTime), Long.toString(numspoken), Long.toString(timespoken));
-            csvWriter3.append(String.join(",", l));
-            csvWriter3.append("\n");
+            csvWriter.close();
         }
-       
-        csvWriter1.close();
-        csvWriter2.close();
-        csvWriter3.close();
     } 
 } 
