92b12aba0a9c80c66173c095b9c2ea3522072b0a
wengcindy <cindyweng66@yahoo.com>
2019-06-04 10:03:24 -0400

    writes csv file

--- jsonParser.java
+++ jsonParser.java
@@ -1,6 +1,11 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.json.*;
 import org.json.simple.JSONArray; 
@@ -10,7 +15,7 @@ import org.json.simple.parser.*;
   
 public class jsonParser 
 { 
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException
     { 
 
         Object obj = null;
@@ -26,11 +31,54 @@ public class jsonParser
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
-           
+		
+		// Set up file and file writer
+		File winter1 = new File("winter1.csv");
+		winter1.createNewFile();
+		File winter2 = new File("winter2.csv");
+		winter2.createNewFile();
+		File winter3 = new File("winter3.csv");
+		winter3.createNewFile();
+        FileWriter csvWriter1 = new FileWriter(winter1); 
+        FileWriter csvWriter2 = new FileWriter(winter2); 
+        FileWriter csvWriter3 = new FileWriter(winter3); 
+
+        
+        // Create column labels for csv
+        csvWriter1.append("Name");  
+        csvWriter1.append(",");  
+        csvWriter1.append("Individual speaking time");  
+        csvWriter1.append(",");  
+        csvWriter1.append("Total number of people spoken");  
+        csvWriter1.append(",");  
+        csvWriter1.append("Total speaking time");   
+        csvWriter1.append("\n");
+        
+        csvWriter2.append("Name");  
+        csvWriter2.append(",");  
+        csvWriter2.append("Individual speaking time");  
+        csvWriter2.append(",");  
+        csvWriter2.append("Total number of people spoken");  
+        csvWriter2.append(",");  
+        csvWriter2.append("Total speaking time");   
+        csvWriter2.append("\n");
+        
+        csvWriter3.append("Name");  
+        csvWriter3.append(",");  
+        csvWriter3.append("Individual speaking time");  
+        csvWriter3.append(",");  
+        csvWriter3.append("Total number of people spoken");  
+        csvWriter3.append(",");  
+        csvWriter3.append("Total speaking time");   
+        csvWriter3.append("\n");
+
         JSONObject jo = (JSONObject) obj; 
         
+        List l = new ArrayList();
         int numspoken = 0;
         int timespoken = 0;
+        
+        // Winter 1
         System.out.println("Winter 1");
         System.out.println("---------");
         JSONObject w1 = (JSONObject) jo.get("2019winter1");
@@ -41,11 +89,17 @@ public class jsonParser
             long endTime = (long) ((JSONObject)o).get("endTime");
             numspoken++;
             timespoken += endTime - startTime;
+            
             System.out.println(username);
             System.out.println(endTime - startTime);
             System.out.println("Num of ppl spoken: " + numspoken);
             System.out.println("Total time spoken: " + timespoken);
             System.out.println();
+            
+            //Write to csv file
+            l = Arrays.asList(username, Long.toString(endTime-startTime), Long.toString(numspoken), Long.toString(timespoken));
+            csvWriter1.append(String.join(",", l));
+            csvWriter1.append("\n");
         }
         System.out.println();
         System.out.println();
@@ -53,6 +107,8 @@ public class jsonParser
         
         numspoken = 0;
         timespoken = 0;
+        
+        // Winter 2
         System.out.println("Winter 2");
         System.out.println("---------");
         JSONObject w2 = (JSONObject) jo.get("2019winter2");
@@ -68,6 +124,10 @@ public class jsonParser
             System.out.println("Num of ppl spoken: " + numspoken);
             System.out.println("Total time spoken: " + timespoken);
             System.out.println();
+            
+            l = Arrays.asList(username, Long.toString(endTime-startTime), Long.toString(numspoken), Long.toString(timespoken));
+            csvWriter2.append(String.join(",", l));
+            csvWriter2.append("\n");
         }
         System.out.println();
         System.out.println();
@@ -75,6 +135,8 @@ public class jsonParser
         
         numspoken = 0;
         timespoken = 0;
+        
+        // Winter 3
         System.out.println("Winter 3");
         System.out.println("---------");
         JSONObject w3 = (JSONObject) jo.get("2019winter3");
@@ -90,6 +152,14 @@ public class jsonParser
             System.out.println("Num of ppl spoken: " + numspoken);
             System.out.println("Total time spoken: " + timespoken);
             System.out.println();
+            
+            l = Arrays.asList(username, Long.toString(endTime-startTime), Long.toString(numspoken), Long.toString(timespoken));
+            csvWriter3.append(String.join(",", l));
+            csvWriter3.append("\n");
         }
+       
+        csvWriter1.close();
+        csvWriter2.close();
+        csvWriter3.close();
     } 
 } 
