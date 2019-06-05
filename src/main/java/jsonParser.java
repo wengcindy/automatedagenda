6d7e42ecdff1bfefbda2a2cc7a95a35cca5a1cd9
Charles Lyu <liang.lyu@duke.edu>
2019-06-05 13:37:36 -0400

    Record stats by section and streamline code

--- src/main/java/jsonParser.java
+++ src/main/java/jsonParser.java
@@ -1,25 +1,34 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-/*import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;*/
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import kong.unirest.UnirestException;
 import org.json.*;
-/*import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
-import org.json.simple.parser.*;*/
   
 public class jsonParser {
     static String path = "2019winter.json";
     static int credits = 0;
+    static boolean VERBOSE = true;
+
+    static List<String> headers = Arrays.asList(
+            "Name",
+            "Individual speaking time",
+            "Individual weighted sentiment score",
+            "Total number of people spoken",
+            "Total speaking time",
+            "Total number of people spoken about pros",
+            "Total speaking time about pros",
+            "Total number of people spoken about cons",
+            "Total speaking time about cons",
+            "Weighted total number of people spoken",
+            "Weighted total speaking time",
+            "Weighted total number of people spoken about pros",
+            "Weighted total speaking time about pros",
+            "Weighted total number of people spoken about cons",
+            "Weighted total speaking time about cons"
+    );
 
     /**
      * Tool for analysis of sentiment and similarities to predetermined
@@ -87,7 +96,7 @@ public class jsonParser {
                 }
             }
 
-            System.out.println(result);
+            //System.out.println(result);
             sentimentTag = result.getString("score_tag");
             // Convert letter score to numeric score
             List<String> sentiments = Arrays.asList("N+", "N", "NEU", "P", "P+");
@@ -97,6 +106,156 @@ public class jsonParser {
             isSubjective = result.getString("subjectivity").equals("SUBJECTIVE");
             sentimentConfidence = result.getInt("confidence");
         }
+    }
+
+    /**
+     * Class that encapsulates all statistics of the ongoing conversation,
+     * including the following data:
+     *
+     * - # of people spoken
+     * - Total speaking time
+     * - # of people spoken about pros
+     * - Total speaking time about pros
+     * - # of people spoken about cons
+     * - Total speaking time about cons
+     * - # of people spoken, weighted by sentiment score (-2 ~ 2)
+     * - Total speaking time, weighted by sentiment score (-2 ~ 2)
+     * - # of people spoken about pros, weighted by sentiment score (1 ~ 2)
+     * - Total speaking time about pros, weighted by sentiment score (1 ~ 2)
+     * - # of people spoken about cons, weighted by sentiment score (1 ~ 2)
+     * - Total speaking time about cons, weighted by sentiment score (1 ~ 2)
+     *
+     * All stats are reset at the start of each topic/section.
+     */
+    public static class StatsRecorder {
+        // For current speech
+        String username = null;
+        double speechTime = 0;
+        double weightedSentiment = 0;
+        // For the entire section
+        int numSpoken = 0;
+        double timeSpoken = 0;
+        double numSpokenPros = 0, timeSpokenPros = 0;
+        double numSpokenCons = 0, timeSpokenCons = 0;
+        double numSpokenWeighted = 0, timeSpokenWeighted = 0;
+        double numSpokenProsWeighted = 0, timeSpokenProsWeighted = 0;
+        double numSpokenConsWeighted = 0, timeSpokenConsWeighted = 0;
+
+        public StatsRecorder() {
+            newSection();
+        }
+
+        public void newSection() {
+            String username = null;
+            speechTime = 0;
+            weightedSentiment = 0;
+            numSpoken = 0;
+            timeSpoken = 0;
+            numSpokenPros = 0; timeSpokenPros = 0;
+            numSpokenCons = 0; timeSpokenCons = 0;
+            numSpokenWeighted = 0; timeSpokenWeighted = 0;
+            numSpokenProsWeighted = 0; timeSpokenProsWeighted = 0;
+            numSpokenConsWeighted = 0; timeSpokenConsWeighted = 0;
+        }
+
+        /**
+         * Start a new speech, i.e. an audio of one person speaking.
+         * @param username Name of current speaker
+         * @param startTime Start time of this speech, as long nubmer
+         * @param endTime End time of this speech, as long nubmer
+         */
+        public void newSpeech(String username, long startTime, long endTime) {
+            this.username = username;
+            speechTime = ((double) endTime - (double) startTime) / 1000;
+            weightedSentiment = 0;
+            numSpoken++;
+            timeSpoken += speechTime;
+        }
+
+        /**
+         * Add a new sentence. Runs sentiment analysis and increment
+         * corresponding counters.
+         *
+         * The increase is according to the proportion this sentence
+         * takes in the person's entire speech in terms of characters.
+         * For example, a 40-character sentence about pros in the
+         * entire speech of 100 characters will contribute 0.4 to
+         * the number of people spoken about pros.
+         * @param sentence Sentence text
+         * @param lengthRatio Proportion of this sentence's length to the
+         *                    total length of that person's speech
+         * @return Score of this sentence
+         */
+        public double addSentence(String sentence, double lengthRatio) {
+            SentimentAnalyzer sa = new SentimentAnalyzer(sentence);
+            int score = sa.sentimentScore;
+            weightedSentiment += lengthRatio * score;
+            numSpokenWeighted += lengthRatio * score;
+            timeSpokenWeighted += speechTime * lengthRatio * score;
+            if (score > 0) {
+                numSpokenPros += lengthRatio;
+                timeSpokenPros += speechTime * lengthRatio;
+                numSpokenProsWeighted += lengthRatio * score;
+                timeSpokenProsWeighted += speechTime * lengthRatio * score;
+            } else if (score < 0) {
+                numSpokenCons += lengthRatio;
+                timeSpokenCons += speechTime * lengthRatio;
+                numSpokenConsWeighted -= lengthRatio * score;
+                timeSpokenConsWeighted -= speechTime * lengthRatio * score;
+            }
+            if (VERBOSE) {
+                System.out.println("Sentiment score: " + score);
+            }
+            return score;
+        }
+
+        /**
+         * Write current statistics to the CSV output file.
+         * Called after each speech.
+         * @param csvWriter CSV writer
+         */
+        public void writeCSV(FileWriter csvWriter) throws IOException {
+            List<String> l = Arrays.asList(
+                    username,
+                    Double.toString(speechTime),
+                    Double.toString(weightedSentiment),
+                    Long.toString(numSpoken),
+                    Double.toString(timeSpoken),
+                    Double.toString(numSpokenPros),
+                    Double.toString(timeSpokenPros),
+                    Double.toString(numSpokenCons),
+                    Double.toString(timeSpokenCons),
+                    Double.toString(numSpokenWeighted),
+                    Double.toString(timeSpokenWeighted),
+                    Double.toString(numSpokenProsWeighted),
+                    Double.toString(timeSpokenProsWeighted),
+                    Double.toString(numSpokenConsWeighted),
+                    Double.toString(timeSpokenConsWeighted)
+            );
+            csvWriter.append(String.join(",", l));
+            csvWriter.append("\n");
+        }
+    }
+
+    /**
+     * Get the time at which each section starts/ends from the JSON
+     * transcript.
+     * Assumes sections are contiguous, but not necessarily in order.
+     * @param sectionData "sectionData" JSON array from the transcript
+     * @return List of start times of each topic * that start with A *
+     *      The last element is the end time of the last agenda
+     */
+    public static List<Long> getSectionTimes(JSONArray sectionData) {
+        Set<Long> startTimes = new TreeSet<>();
+        long endTime = 0;
+        for (int i=0; i<sectionData.length(); i++) {
+            JSONObject section = sectionData.getJSONObject(i);
+            startTimes.add(section.getLong("startTime"));
+            endTime = Math.max(endTime, section.getLong("endTime"));
+        }
+        List<Long> times = new ArrayList<>(startTimes);
+        times.add(endTime);
+        return times;
     }
 
     public static void main(String[] args) throws IOException {
@@ -110,52 +269,47 @@ public class jsonParser {
 
 		for (Object sessionObj : database.keySet()) {
             String sessionName = (String) sessionObj;
+            JSONObject sessionData = database.getJSONObject(sessionName);
+            JSONArray sessionAudio = sessionData.getJSONArray("audioData");
+            List<Long> sectionTimes = getSectionTimes(sessionData.getJSONArray("sectionData"));
+            if (sectionTimes.isEmpty()) {
+                continue;
+            }
+
             System.out.println(sessionName);
             System.out.println("---------");
-
             File file = new File(sessionName + ".csv");
             file.createNewFile();
             FileWriter csvWriter = new FileWriter(file);
 
             // Create column labels for csv
-            List<String> headers = Arrays.asList(
-                    "Name",
-                    "Individual speaking time",
-                    "Individual weighted sentiment score",
-                    "Total number of people spoken",
-                    "Total speaking time",
-                    "Total number of people spoken about pros",
-                    "Total speaking time about pros",
-                    "Total number of people spoken about cons",
-                    "Total speaking time about cons",
-                    "Weighted total number of people spoken",
-                    "Weighted total speaking time",
-                    "Weighted total number of people spoken about pros",
-                    "Weighted total speaking time about pros",
-                    "Weighted total number of people spoken about cons",
-                    "Weighted total speaking time about cons"
-            );
             csvWriter.append(String.join(",", headers) + "\n");
 
-            int numSpoken = 0;
-            double timeSpoken = 0;
-            double numSpokenPros = 0, timeSpokenPros = 0;
-            double numSpokenCons = 0, timeSpokenCons = 0;
-            double numSpokenWeighted = 0, timeSpokenWeighted = 0;
-            double numSpokenProsWeighted = 0, timeSpokenProsWeighted = 0;
-            double numSpokenConsWeighted = 0, timeSpokenConsWeighted = 0;
-            JSONObject sessionData = database.getJSONObject(sessionName);
-            JSONArray sessionAudio = sessionData.getJSONArray("audioData");
+            int currentSection = 0;
+            StatsRecorder stats = new StatsRecorder();
 
             for (int i=0; i<sessionAudio.length(); i++){
                 JSONObject singleSpeech = sessionAudio.getJSONObject(i);
                 String username = singleSpeech.getString("username");
-                double startTime = singleSpeech.getLong("startTime")/1000.0;
-                double endTime = singleSpeech.getLong("endTime")/1000.0;
-                double speechTime = endTime - startTime;
-                double weightedSentiment = 0;
-                numSpoken++;
-                timeSpoken += speechTime;
+                long startTime = singleSpeech.getLong("startTime");
+                long endTime = singleSpeech.getLong("endTime");
+
+                // Checks if a new section needs to be started, and whether
+                // this speech should count at all
+                if (startTime < sectionTimes.get(currentSection)) {
+                    continue;  // Intro (before A1)
+                } else if (startTime >= sectionTimes.get(currentSection + 1)) {
+                    currentSection += 1;
+                    if (currentSection == sectionTimes.size() - 1) {
+                        break;  // Past the last section (question generation, sorting)
+                    }
+                    stats.newSection();
+                    System.out.println("---------");
+                    System.out.println("New section");
+                    System.out.println("---------");
+                }
+
+                stats.newSpeech(username, startTime, endTime);
 
                 // Each audio might be broken down into several sentences.
                 // Extract all sentences
@@ -169,71 +323,16 @@ public class jsonParser {
                     totalLength += sentence.length();
                 }
 
-                /* Analyze the sentiment of each sentence, and then increment
-                 the # people, speaking time and weighted sentiment score
-                 accordingly.
-                 The increase is according to the proportion this sentence
-                 takes in the entire speech in terms of characters.
-                 For example, a 40-character sentence about pros in the
-                 entire speech of 100 characters will contribute 0.4 to
-                 the number of people spoken about pros.*/
                 for (String sentence : sentences) {
-                    SentimentAnalyzer sa = new SentimentAnalyzer(sentence);
-                    int score = sa.sentimentScore;
                     double ratio = sentence.length() * 1.0 / totalLength;
-                    weightedSentiment += ratio * score;
-                    numSpokenWeighted += ratio * score;
-                    timeSpokenWeighted += speechTime * ratio * score;
-                    if (score > 0) {
-                        numSpokenPros += ratio;
-                        timeSpokenPros += speechTime * ratio;
-                        numSpokenProsWeighted += ratio * score;
-                        timeSpokenProsWeighted += speechTime * ratio * score;
-                    } else if (score < 0) {
-                        numSpokenCons += ratio;
-                        timeSpokenCons += speechTime * ratio;
-                        numSpokenConsWeighted -= ratio * score;
-                        timeSpokenConsWeighted -= speechTime * ratio * score;
-                    }
+                    System.out.println(sentence);
+                    stats.addSentence(sentence, ratio);
+                    System.out.println();
+
+                    // Labeling tools to be added
                 }
 
-                System.out.println(username);
-                System.out.println("Time of this speech:" + speechTime);
-                System.out.println("Weighted sentiment score:" + weightedSentiment);
-                System.out.println("Num of ppl spoken: " + numSpoken);
-                System.out.println("Total time spoken: " + timeSpoken);
-                System.out.println("Num of ppl spoken weighted: " + numSpokenWeighted);
-                System.out.println("Total time spoken weighted: " + timeSpokenWeighted);
-                System.out.println("Num of ppl spoken pros: " + numSpokenPros);
-                System.out.println("Total time spoken pros: " + timeSpokenPros);
-                System.out.println("Num of ppl spoken cons: " + numSpokenCons);
-                System.out.println("Total time spoken cons: " + timeSpokenCons);
-                System.out.println("Num of ppl spoken pros weighted: " + numSpokenProsWeighted);
-                System.out.println("Total time spoken pros weighted: " + timeSpokenProsWeighted);
-                System.out.println("Num of ppl spoken cons weighted: " + numSpokenConsWeighted);
-                System.out.println("Total time spoken cons weighted: " + timeSpokenConsWeighted);
-                System.out.println();
-
-                // Write to csv file
-                List<String> l = Arrays.asList(
-                        username,
-                        Double.toString(speechTime),
-                        Double.toString(weightedSentiment),
-                        Long.toString(numSpoken),
-                        Double.toString(timeSpoken),
-                        Double.toString(numSpokenPros),
-                        Double.toString(timeSpokenPros),
-                        Double.toString(numSpokenCons),
-                        Double.toString(timeSpokenCons),
-                        Double.toString(numSpokenWeighted),
-                        Double.toString(timeSpokenWeighted),
-                        Double.toString(numSpokenProsWeighted),
-                        Double.toString(timeSpokenProsWeighted),
-                        Double.toString(numSpokenConsWeighted),
-                        Double.toString(timeSpokenConsWeighted)
-                );
-                csvWriter.append(String.join(",", l));
-                csvWriter.append("\n");
+                stats.writeCSV(csvWriter);
             }
             System.out.println();
             System.out.println();
