d7dff850017289acc9afad10f6421dadd4b7c3a6
Charles Lyu <liang.lyu@duke.edu>
2019-06-12 13:48:09 -0400

    Labeling of conversation labels; Preparation for labeling sentence sentiments

--- src/main/java/jsonParser.java
+++ src/main/java/jsonParser.java
@@ -13,15 +13,18 @@ import org.json.*;
 public class jsonParser {
     final static boolean VERBOSE = true;
     final static boolean IGNORE_EMPTY_MESSAGES = true;
-    final static boolean IMPORT_LABELS = true;  // true;
-    final static String path = "2019winter.json";
+    static boolean IMPORT_LABELS = true;
+    static boolean IMPORT_PROS_CONS = true;
+    final static String path = "climate.json";
     final static String oldCSVPath = ".";
 
     static int credits = 0;
     static Map<String, Map<Integer, Integer>> oldLabels = new HashMap<>();
+    static Map<String, Map<Integer, Map<Integer, Integer>>> oldSentiments = new HashMap<>();
 
     static List<String> headers = Arrays.asList(
-            "ID",
+            "Section ID",
+            "Speech ID",
             "Name",
             "Individual speaking time",
             "Individual sentiment",
@@ -38,6 +41,15 @@ public class jsonParser {
             "Cons # ppl spoken (weighted)",
             "Cons speaking time (weighted)",
             "Label of current status"
+    );
+    static List<String> transcriptHeaders = Arrays.asList(
+            "Section ID",
+            "Speech ID",
+            "Sentence ID",
+            "Name",
+            "Text",
+            "MeaningCloud sentiment",
+            "True sentiment"
     );
 
     /**
@@ -165,13 +177,19 @@ public class jsonParser {
      */
     public static class StatsRecorder {
         String sessionName = null;
+
         // For current speech
         String username = null;
         int audioId = 0;
         double speechTime = 0;
         double weightedSentiment = 0;
         int label = 0;
+        List<String> sentences = new ArrayList<>();
+        List<Double> predictedSentiments = new ArrayList<>();
+        List<Integer> trueSentiments = new ArrayList<>();
+
         // For the entire section
+        int sectionId = 0;
         int numSpoken = 0;
         double timeSpoken = 0;
         //double numSpokenPros = 0, timeSpokenPros = 0;
@@ -180,14 +198,18 @@ public class jsonParser {
         double numSpokenProsWeighted = 0, timeSpokenProsWeighted = 0;
         double numSpokenConsWeighted = 0, timeSpokenConsWeighted = 0;
         FileWriter csvWriter = null;
+        FileWriter transcriptWriter = null;
 
-        public StatsRecorder(String session, FileWriter csvWriter) {
+        public StatsRecorder(String session, FileWriter csvWriter, FileWriter transcriptWriter) throws IOException {
             sessionName = session;
             this.csvWriter = csvWriter;
+            this.transcriptWriter = transcriptWriter;
+            sectionId = -1;  // incremented to 0 in newSection
             newSection();
         }
 
-        public void newSection() {
+        public void newSection() throws IOException {
+            sectionId += 1;
             username = null;
             audioId = 0;
             speechTime = 0;
@@ -199,6 +221,8 @@ public class jsonParser {
             numSpokenWeighted = 0; timeSpokenWeighted = 0;
             numSpokenProsWeighted = 0; timeSpokenProsWeighted = 0;
             numSpokenConsWeighted = 0; timeSpokenConsWeighted = 0;
+            csvWriter.flush();
+            transcriptWriter.flush();
         }
 
         /**
@@ -215,7 +239,7 @@ public class jsonParser {
          * @param startTime Start time of this speech, as long nubmer
          * @param endTime End time of this speech, as long nubmer
          */
-        public void addSpeech(int id, String user, String speech, long startTime, long endTime) {
+        public void addSpeech(int id, String user, String speech, long startTime, long endTime) throws IOException {
             username = user;
             audioId = id;
             speechTime = ((double) endTime - (double) startTime) / 1000;
@@ -224,6 +248,8 @@ public class jsonParser {
             timeSpoken += speechTime;
             int length = speech.length();
 
+            sentences = new ArrayList<>();
+            predictedSentiments = new ArrayList<>();
             SentimentAnalyzer sa = new SentimentAnalyzer(speech);
             if (sa.sentences.isEmpty()) {
                 addSentence(sa, 1.0);
@@ -233,7 +259,10 @@ public class jsonParser {
                 }
             }
 
-            readLabel();
+            label = readLabel();
+            trueSentiments = readTrueSentiment();
+            writeCSV();
+            writeTranscript();
         }
 
         /**
@@ -249,9 +278,11 @@ public class jsonParser {
          *                    total length of that person's speech
          * @return Score of this sentence
          */
-        public int addSentence(SentimentAnalyzer sa, double lengthRatio) {
+        public double addSentence(SentimentAnalyzer sa, double lengthRatio) throws IOException {
+            sentences.add(sa.message);
             System.out.println(username + ": " + sa.message);
-            int score = sa.sentimentScore;
+            double score = sa.sentimentScore;
+            predictedSentiments.add(score);
             weightedSentiment += lengthRatio * score;
             numSpokenWeighted += lengthRatio * score;
             timeSpokenWeighted += speechTime * lengthRatio * score;
@@ -275,24 +306,71 @@ public class jsonParser {
         /**
          * Reads the label for current speech (status at the end of this),
          * either from user input or from existing sources.
+         *
+         * @return Label
          */
-        public void readLabel() {
+        public int readLabel() {
+            int l = 0;
             if (IMPORT_LABELS) {
                 boolean success = true;
                 try {
-                    label = oldLabels.get(sessionName).get(audioId);
+                    l = oldLabels.get(sessionName).get(audioId);
                 } catch (NullPointerException e) {
                     // ID key doesn't exist for some reason
                     success = false;
                 }
                 if (success) {
-                    return;
+                    return l;
                 }
             }
             Scanner in = new Scanner(System.in);
-            System.out.print("Please enter label (0 - insufficient in both pros and cons, 1 - sufficient pros, 2 - sufficient cons, 3 - sufficient in both pros and cons): ");
-            label = in.nextInt();
+            //System.out.print("Please enter label (0 - insufficient in both pros and cons, 1 - sufficient pros, 2 - sufficient cons, 3 - sufficient in both pros and cons): ");
+            //label = in.nextInt();
+            System.out.print("Please enter label: 1 for sufficient pros, 2 for sufficient cons, 3 or 12 for sufficient in both: ");
+            String s = in.nextLine();
+            if (s.contains("3")) {
+                l = 3;
+            } else {
+                l = (s.contains("1") ? 1 : 0) + (s.contains("2") ? 2 : 0);
+            }
             System.out.println();
+            return l;
+        }
+
+        /**
+         * Reads the true sentiment for current sentence,
+         * either from user input or from existing sources.
+         *
+         * @return List of true sentiment scores (-1 or 1) for each sentence
+         */
+        public List<Integer> readTrueSentiment() {
+            int sentenceNum = sentences.size();
+            List<Integer> scores = new ArrayList<>();
+            if (IMPORT_PROS_CONS) {
+                boolean success = true;
+                try {
+                    for (int i=0; i<sentenceNum; i++) {
+                        scores.add(oldSentiments.get(sessionName).get(audioId).get(i));
+                    }
+                } catch (NullPointerException e) {
+                    // ID key doesn't exist for some reason
+                    success = false;
+                }
+                if (success) {
+                    return scores;
+                }
+            }
+            Scanner in = new Scanner(System.in);
+            String s = "";
+            while (s.length() != sentenceNum) {
+                System.out.print("Please enter " + sentenceNum + " sentiment scores: 1 for positive, 0 for negative, space for neutral (e.g.1 010):");
+                s = in.nextLine();
+            }
+            for (char c : s.toCharArray()) {
+                scores.add(c == '1'? 1: (c == '0'? -1: 0));
+            }
+            System.out.println();
+            return scores;
         }
 
         /**
@@ -301,6 +379,7 @@ public class jsonParser {
          */
         public void writeCSV() throws IOException {
             List<String> l = Arrays.asList(
+                    Integer.toString(sectionId),
                     Integer.toString(audioId),
                     username,
                     Double.toString(speechTime),
@@ -322,6 +401,26 @@ public class jsonParser {
             csvWriter.append(String.join(",", l));
             csvWriter.append("\n");
         }
+
+        /**
+         * Write transcripts and sentiment scores to the CSV output file.
+         * Called after each speech, but adds all sentences in separate entries.
+         */
+        public void writeTranscript() throws IOException {
+            for (int i=0; i<sentences.size(); i++) {
+                List<String> l = Arrays.asList(
+                        Integer.toString(sectionId),
+                        Integer.toString(audioId),
+                        Integer.toString(i),
+                        username,
+                        "\"" + sentences.get(i) + "\"",  // Use "" to escape commas
+                        Double.toString(predictedSentiments.get(i)),
+                        Integer.toString(trueSentiments.get(i))
+                );
+                transcriptWriter.append(String.join(",", l));
+                transcriptWriter.append("\n");
+            }
+        }
     }
 
     /**
@@ -335,7 +434,10 @@ public class jsonParser {
         CSVReader csvReader = new CSVReader(new FileReader(oldCSV.toFile()));
 
         List<String> header = Arrays.asList(csvReader.readNext());
-        int idHeader = header.indexOf("ID");
+        int idHeader = header.indexOf("Speech ID");
+        if (idHeader == -1) {
+            idHeader = header.indexOf("ID");  // Compatibility
+        }
         int labelHeader = header.indexOf("Label of current status");
         if (idHeader == -1) {
             throw new IOException("Malformatted CSV file: ID header not found");
@@ -348,6 +450,43 @@ public class jsonParser {
         String[] row = csvReader.readNext();
         while (row != null) {
             oldLabels.get(sessionName).put(Integer.parseInt(row[idHeader]),
+                    Integer.parseInt(row[labelHeader]));
+            row = csvReader.readNext();
+        }
+        csvReader.close();
+    }
+
+    /**
+     * Read true sentiments from past CSVs, if applicable.
+     * Assumes CSV file is stored in oldCSVPath and named as the session
+     * name concatenated with "_transcript.csv".
+     * @param sessionName Name of session (room)
+     */
+    public static void initializeTranscripts(String sessionName) throws IOException {
+        Path oldCSV = Paths.get(oldCSVPath).resolve(sessionName + "_transcript.csv");
+        CSVReader csvReader = new CSVReader(new FileReader(oldCSV.toFile()));
+
+        List<String> header = Arrays.asList(csvReader.readNext());
+        int speechIdHeader = header.indexOf("Speech ID");
+        int sentenceIdHeader = header.indexOf("Sentence ID");
+        int labelHeader = header.indexOf("True sentiment");
+        if (speechIdHeader == -1) {
+            throw new IOException("Malformatted CSV file: speech ID header not found");
+        }
+        if (sentenceIdHeader == -1) {
+            throw new IOException("Malformatted CSV file: sentence ID header not found");
+        }
+        if (labelHeader == -1) {
+            throw new IOException("Malformatted CSV file: label header not found");
+        }
+
+        oldSentiments.putIfAbsent(sessionName, new HashMap<>());
+        String[] row = csvReader.readNext();
+        while (row != null) {
+            int speechId = Integer.parseInt(row[speechIdHeader]);
+            int sentenceId = Integer.parseInt(row[sentenceIdHeader]);
+            oldSentiments.get(sessionName).putIfAbsent(speechId, new HashMap<>());
+            oldSentiments.get(sessionName).get(speechId).put(sentenceId,
                     Integer.parseInt(row[labelHeader]));
             row = csvReader.readNext();
         }
@@ -388,7 +527,8 @@ public class jsonParser {
      */
     public static void processSpeech(JSONObject singleSpeech, StatsRecorder stats) throws IOException {
         int id = singleSpeech.getInt("id");
-        String username = singleSpeech.getString("username");
+        String username = singleSpeech.getString("username")
+                .replaceAll(",", "");  // Prevent messing up with CSV
         long startTime = singleSpeech.getLong("startTime");
         long endTime = singleSpeech.getLong("endTime");
 
@@ -408,7 +548,6 @@ public class jsonParser {
         }
 
         stats.addSpeech(id, username, speech, startTime, endTime);
-        stats.writeCSV();
     }
 
     /**
@@ -429,16 +568,23 @@ public class jsonParser {
         if (IMPORT_LABELS) {  // IMPORTANT: Do this before writing the new CSV file since it might override old data
             initializeLabels(sessionName);
         }
+        if (IMPORT_PROS_CONS) {  // IMPORTANT: Do this before writing the new CSV file since it might override old data
+            initializeTranscripts(sessionName);
+        }
 
         File file = new File(sessionName + ".csv");
         file.createNewFile();
         FileWriter csvWriter = new FileWriter(file);
+        File transcriptFile = new File(sessionName + "_transcript.csv");
+        transcriptFile.createNewFile();
+        FileWriter transcriptWriter = new FileWriter(transcriptFile);
 
         // Create column labels for csv
         csvWriter.append(String.join(",", headers) + "\n");
+        transcriptWriter.append(String.join(",", transcriptHeaders) + "\n");
 
         int currentSection = 0;
-        StatsRecorder stats = new StatsRecorder(sessionName, csvWriter);
+        StatsRecorder stats = new StatsRecorder(sessionName, csvWriter, transcriptWriter);
 
         for (int i=0; i<sessionAudio.length(); i++){
             JSONObject singleSpeech = sessionAudio.getJSONObject(i);
@@ -465,9 +611,18 @@ public class jsonParser {
         System.out.println();
         System.out.println();
         csvWriter.close();
+        transcriptWriter.close();
     }
 
     public static void main(String[] args) throws IOException {
+        System.out.print("Do you want to input conversation labels manually? (Y/N):");
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        IMPORT_LABELS = !(s.equals("Y"));
+        System.out.print("Do you want to input true sentiment (pros/cons) labels manually? (Y/N):");
+        s = in.nextLine();
+        IMPORT_PROS_CONS = !(s.equals("Y"));
+
         JSONObject database = null;
         InputStream is = jsonParser.class.getResourceAsStream(path);
         if (is == null) {
