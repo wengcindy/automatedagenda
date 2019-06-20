55be4700a1269c5b2bb2c52468a450075fdf0f42
Charles Lyu <liang.lyu@duke.edu>
2019-06-20 15:18:23 -0400

    Start migrating Java parser to Python; Clean up code Part I

--- transcriptParser.py
+++ transcriptParser.py
@@ -1,9 +1,17 @@
 import json
 import csv
 
+VERBOSE = True
+IGNORE_EMPTY_MESSAGES = True
+IMPORT_LABELS = True
+IMPORT_PROS_CONS = True
+filename = "20190430-finance"
+old_CSV_path = "."
+
+
 def read(filename):
 	with open(filename + '.json', 'r') as myfile:
-	    transcript=myfile.read()
+		transcript = myfile.read()
 
 	# parse json
 	obj = json.loads(transcript)
@@ -12,53 +20,68 @@ def read(filename):
 	csvWriter = csv.writer(out)
 	csvWriter.writerow(["username", "text", "start time", "end time","total time", "session", "section"])
 
-	# list of all sessions
-	sessions = []
-	for session in obj:
-		sessions.append(session)
+	for session_name, session_obj in obj.items():
+		sectionDict = get_section_times(session_obj['sectionData'])
 
-	# get stats from each session
-	for session in obj:
-		sectionDict = makeDictionary(obj, session)
-		for audio in range(len(obj[session]['audioData'])):
-			for j in range(len(obj[session]['audioData'][audio]['data'])):
+		# TODO: Import labels, pros/cons
+
+		# TODO: Create transcript CSV
+
+		for audio in range(len(session_obj['audioData'])):
+			for j in range(len(session_obj['audioData'][audio]['data'])):
 				# print(obj['2019winter2']['audioData'][audio]['data'][j]['text'])
 				# print("\n")
-				csvWriter.writerow([obj[session]['audioData'][audio]['username'], \
-					obj[session]['audioData'][audio]['data'][j]['text'], \
-					obj[session]['audioData'][audio]['startTime'], \
-					obj[session]['audioData'][audio]['endTime'], \
-					obj[session]['audioData'][audio]['endTime']/1000 - obj[session]['audioData'][audio]['startTime']/1000, \
-					session, \
-					matchSection(sectionDict, obj[session]['audioData'][audio]['startTime'], obj[session]['audioData'][audio]['endTime'])])
+				csvWriter.writerow([session_obj['audioData'][audio]['username'], \
+					session_obj['audioData'][audio]['data'][j]['text'], \
+					session_obj['audioData'][audio]['startTime'], \
+					session_obj['audioData'][audio]['endTime'], \
+					session_obj['audioData'][audio]['endTime']/1000 - session_obj['audioData'][audio]['startTime']/1000, \
+					session_name, \
+					match_section(sectionDict, session_obj['audioData'][audio]['startTime'], session_obj['audioData'][audio]['endTime'])])
 
-# create dictionary for each session
-def makeDictionary(obj, session):
-	# key: section name (A1, A2, ...), value: array with start and end times
-	sectionDict = dict()
 
-	for section in range(len(obj[session]['sectionData'])):
-		if(obj[session]['sectionData'][section]['name'].startswith('A')):
+def get_section_times(section_data):
+	"""
+	Get the time at which each section starts/ends from the JSON transcript.
+	:param section_data: "sectionData" JSON array from the transcript
+	:return: Dict mapping section names that start with A (A1, A2, ...)
+		to list of start and end times (can have multiple pairs of them)
+	"""
+	section_dict = dict()
+
+	for section in section_data:
+		section_name = section['name']
+		section_list = [section['startTime'], section['endTime']]
+		if (section_name.startswith('A')):
 			# append the time to the existing array for the section
-			if obj[session]['sectionData'][section]['name'] in sectionDict:
-				sectionDict[obj[session]['sectionData'][section]['name']].append(obj[session]['sectionData'][section]['startTime'])
-				sectionDict[obj[session]['sectionData'][section]['name']].append(obj[session]['sectionData'][section]['endTime'])
+			if section_name in section_dict:
+				section_dict[section_name] += section_list
 			# create a new array for this section
 			else:
-				sectionList = []
-				sectionList.append(obj[session]['sectionData'][section]['startTime'])
-				sectionList.append(obj[session]['sectionData'][section]['endTime'])
-				sectionDict[obj[session]['sectionData'][section]['name']] = sectionList
-	return sectionDict
+				section_dict[section_name] = section_list
+	return section_dict
 
-# match user's start/end time to a section
-def matchSection(sectionDict, start, end):
-	for i in sectionDict:
-		if start >= sectionDict[i][0] and end <= sectionDict[i][1]:
-			return i
 
-filename = '20190430-finance'
-read(filename)
+def match_section(section_dict, start, end):
+	"""
+	Find the section of a user's speech.
+	:param section_dict: Dict mapping section names to start and end times
+	:param start: Start time of user's speech
+	:param end: End time of user's speech
+	:return: Section name, or None if it's not in any section
+	"""
+	for section_name, times in section_dict.items():
+		for j in range(int(len(times) / 2)):
+			if start >= times[j*2] and end <= times[j*2+1]:
+				return section_name
+	return None
+
+
+if __name__ == "__main__":
+	IMPORT_LABELS = (input("Do you want to input conversation labels manually? (Y/N):") == "Y")
+	IMPORT_PROS_CONS = (input("Do you want to input true sentiment (pros/cons) labels manually? (Y/N):") == "Y")
+
+	read(filename)
 
 
 
